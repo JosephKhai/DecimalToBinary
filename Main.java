3c5f2dbda1ad047b85723f873a682bb3971b40cd
Pum Suan Khai SEKTAK <39785561+pumsuankhaiSEKTAK@users.noreply.github.com>
2022-07-14 14:14:38 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+		Main main = new Main();
+		var result = main.decimalToBinary(10);
+    System.out.println(result);
+  }
+
+	public int decimalToBinary(int n){
+		if(n==0){
+			return 0;
+		}
+		return n % 2 + 10 * decimalToBinary(n/2);
+	}
+}
